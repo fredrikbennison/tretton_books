a8e69806e76aeef186523dcc1e75becad5a9b362
Fredrik Bennison <fredrik.bennison@lfv.se>
2024-01-17 20:29:34 +0100

    Added recursive parsing of html pages for links and sources

--- tretton_books.py
+++ tretton_books.py
@@ -4,18 +4,28 @@ from bs4 import BeautifulSoup
 
 site = "https://books.toscrape.com/"
 
-async def pageparser(session, url, pgqueue, sources):
-    async with session.get(url) as response:
+async def pageparser(session, pgqueue, sources):
+    while True:
+        # Retrieve URL from queue
+        url = await pgqueue.get()
 
-        html = await response.text()
-        soup = BeautifulSoup(html, "html.parser")
-
-        # Find all links
-        for alink in soup.find_all('a'):
-            print("a: ", alink.attrs['href'])
-            new_url = site + response.url.path + alink.attrs['href']
-            print(new_url)
-            await pgqueue.put(new_url)
+        print("Parsing: ", url)
+        async with session.get(url) as response:
+    
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+    
+            # Find all links
+            for alink in soup.find_all('a'):
+                print("a: ", alink.attrs['href'])
+                new_url = site + response.url.parent.path + alink.attrs['href']
+                print(new_url)
+                await pgqueue.put(new_url)
+                
+            for source in soup.find_all('script'): 
+                if (source.has_attr('src')):
+                    print("src: ", source.attrs['src'])
+                
 
     
 async def main():
@@ -23,5 +33,7 @@ async def main():
     async with aiohttp.ClientSession() as session:
         parserQueue = asyncio.Queue()
         sourceQueue = asyncio.Queue()
-        await pageparser(session, site, parserQueue, sourceQueue)
+        await parserQueue.put(site)
+        await pageparser(session, parserQueue, sourceQueue)
+        
 asyncio.run(main())
