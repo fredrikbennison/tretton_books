7698d369095cc8305f916888b459ee5bd3338a7c
Fredrik Bennison <fredrik.bennison@gmail.com>
2024-01-20 20:23:55 +0100

    Added CS project and first HTML connection

---
+++ tretton_books/tretton_books/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+
+string baseUri = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html";
+string outputDir = @"C:\Temp";
+
+BlockingCollection<Uri> parserQueue = [];
+BlockingCollection<Uri> sourceQueue = [];
+ConcurrentDictionary<Uri, int> seenUris = new();
+HttpClient client = new();
+
+async Task parsePage()
+{
+
+    Uri uri = new(baseUri);
+
+    while (sourceQueue.TryTake(out uri))
+    {
+        using HttpResponseMessage response = await client.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(responseBody[..10]);
+    }
+}
+
+sourceQueue.Add(new Uri(baseUri));
+var tasks = new List<Task>() {parsePage()};
+
+await Task.WhenAll(tasks);
+
+Console.WriteLine("Download complete");
