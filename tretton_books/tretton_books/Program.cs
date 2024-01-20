59b2abb7deb5d76fe146a1392b7293a7ecccaa65
Fredrik Bennison <fredrik.bennison@gmail.com>
2024-01-20 23:07:54 +0100

    Misinterpreted the use of BlockingCollection, changed to Channel for asynchronous operation

--- tretton_books/tretton_books/Program.cs
+++ tretton_books/tretton_books/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Concurrent;
-using System.Reflection.Metadata;
+using System.Threading.Channels;
 using HtmlAgilityPack;
 
 string baseUri = "https://books.toscrape.com/";
 string outputDir = @"C:\Temp\";
 
-BlockingCollection<Uri> parserQueue = [];
-BlockingCollection<Uri> sourceQueue = [];
+Channel<Uri> parserChannel = Channel.CreateUnbounded<Uri>();
+Channel<Uri> sourceChannel = Channel.CreateUnbounded<Uri>();
 ConcurrentDictionary<Uri, int> seenUris = new();
 HttpClient client = new();
 
@@ -15,7 +15,7 @@ async Task downloadSource()
     while (true)
     {
         try {
-            Uri uri = sourceQueue.Take();
+            Uri uri = await sourceChannel.Reader.ReadAsync();
             // Only parse new uris
             if (seenUris.TryAdd(uri, 0))
             {
@@ -24,7 +24,7 @@ async Task downloadSource()
                 {
                     info = info[^80..];
                 }
-                Console.Write(String.Format("\rDownloading\t\t{0,-80}", info));
+                Console.Write(String.Format("\rDownloading\t{0,-80}", info));
                 using HttpResponseMessage response = await client.GetAsync(uri);
                 response.EnsureSuccessStatusCode();
 
@@ -44,79 +44,90 @@ async Task downloadSource()
 
 async Task parsePage()
 {
-    while (parserQueue.TryTake(out Uri? uri))
+    while (true)
     {
-        // Only parse new uris
-        if (seenUris.TryAdd(uri, 0))
-        {
-            string info = uri.ToString();
-            if (info.Length > 80)
+        try {
+            Uri uri = await parserChannel.Reader.ReadAsync();
+            // Only parse new uris
+            if (seenUris.TryAdd(uri, 0))
             {
-                info = info[^80..];
-            }
-            Console.Write(String.Format("\rParsing\t\t{0,-80}", info));
-            using HttpResponseMessage response = await client.GetAsync(uri);
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-
-            HtmlDocument html = new();
-            html.LoadHtml(responseBody);
-
-            // Check all a link tags
-            foreach (var tag in html.DocumentNode.Descendants("a"))
-            {
-                if (tag.Attributes.Contains("href"))
+                string info = uri.ToString();
+                if (info.Length > 80)
                 {
-                    parserQueue.Add(new Uri(uri, tag.GetAttributeValue("href", "")));
+                    info = info[^80..];
                 }
-            }
+                Console.Write(String.Format("\rParsing\t\t{0,-80}", info));
+                using HttpResponseMessage response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
 
-            // Check all source tags
-            foreach (var tag in html.DocumentNode.Descendants("script"))
-            {
-                if (tag.Attributes.Contains("src") && !tag.GetAttributeValue("src", "").StartsWith("http"))
-                {
-                    sourceQueue.Add(new Uri(uri, tag.GetAttributeValue("src", "")));
-                }
-            }
-            // Check all img tags
-            foreach (var tag in html.DocumentNode.Descendants("img"))
-            {
-                if (tag.Attributes.Contains("src") && !tag.GetAttributeValue("src", "").StartsWith("http"))
-                {
-                    sourceQueue.Add(new Uri(uri, tag.GetAttributeValue("src", "")));
-                }
-            }
-            // Check all link tags
-            foreach (var tag in html.DocumentNode.Descendants("link"))
-            {
-                if (tag.Attributes.Contains("href") && !tag.GetAttributeValue("href", "").StartsWith("http"))
-                {
-                    sourceQueue.Add(new Uri(uri, tag.GetAttributeValue("href", "")));
-                }
-            }
+                HtmlDocument html = new();
+                html.LoadHtml(responseBody);
 
-            var filePath = Path.Combine(outputDir, uri.AbsolutePath[1..]);
-            if (Path.GetFileName(filePath)?.Length == 0) {
-                filePath = Path.Combine(filePath, "index.html");
-            }
+                // Check all a link tags
+                foreach (var tag in html.DocumentNode.Descendants("a"))
+                {
+                    if (tag.Attributes.Contains("href"))
+                    {
+                        await parserChannel.Writer.WriteAsync(new Uri(uri, tag.GetAttributeValue("href", "")));
+                    }
+                }
 
-            var dir = Path.GetDirectoryName(filePath);
-            if (dir != null) {
-                Directory.CreateDirectory(dir);
+                // Check all source tags
+                foreach (var tag in html.DocumentNode.Descendants("script"))
+                {
+                    if (tag.Attributes.Contains("src") && !tag.GetAttributeValue("src", "").StartsWith("http"))
+                    {
+                        await sourceChannel.Writer.WriteAsync(new Uri(uri, tag.GetAttributeValue("src", "")));
+                    }
+                }
+                // Check all img tags
+                foreach (var tag in html.DocumentNode.Descendants("img"))
+                {
+                    if (tag.Attributes.Contains("src") && !tag.GetAttributeValue("src", "").StartsWith("http"))
+                    {
+                        await sourceChannel.Writer.WriteAsync(new Uri(uri, tag.GetAttributeValue("src", "")));
+                    }
+                }
+                // Check all link tags
+                foreach (var tag in html.DocumentNode.Descendants("link"))
+                {
+                    if (tag.Attributes.Contains("href") && !tag.GetAttributeValue("href", "").StartsWith("http"))
+                    {
+                        await sourceChannel.Writer.WriteAsync(new Uri(uri, tag.GetAttributeValue("href", "")));
+                    }
+                }
+
+                var filePath = Path.Combine(outputDir, uri.AbsolutePath[1..]);
+                if (Path.GetFileName(filePath)?.Length == 0) {
+                    filePath = Path.Combine(filePath, "index.html");
+                }
+
+                var dir = Path.GetDirectoryName(filePath);
+                if (dir != null) {
+                    Directory.CreateDirectory(dir);
+                }
+                await File.WriteAllTextAsync(filePath, responseBody);
             }
-            await File.WriteAllTextAsync(filePath, responseBody);
+        }
+        catch (ChannelClosedException) {
+            break;
+        }
+
+        if (parserChannel.Reader.Count == 0) {
+            // No more pages to parse, mark channel as completed
+            parserChannel.Writer.Complete();
         }
     }
 }
 
-parserQueue.Add(new Uri(baseUri));
+await parserChannel.Writer.WriteAsync(new Uri(baseUri));
 var parseTasks = new List<Task>() {parsePage(), parsePage(), parsePage()};
 var downloadTasks = new List<Task>() {downloadSource(), downloadSource(), downloadSource()};
 await Task.WhenAll(parseTasks);
 
 // Parsing complete, no new sources will be added
-sourceQueue.CompleteAdding();
+sourceChannel.Writer.Complete();
 
 await Task.WhenAll(downloadTasks);
 
